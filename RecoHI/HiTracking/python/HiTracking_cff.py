9a8ea4f7e2df6b8bd6407da0b4e4118485badcea
Doga <dgulhan@cern.ch>
2015-08-12 09:42:16 +0200

    parameters set

--- RecoHI/HiTracking/python/HiTracking_cff.py
+++ RecoHI/HiTracking/python/HiTracking_cff.py
@@ -6,14 +6,20 @@ from RecoHI.HiTracking.hiMixedTripletStep_cff import *
 from RecoHI.HiTracking.hiPixelPairStep_cff import *
 from RecoHI.HiTracking.hiDetachedTripletStep_cff import *
 from RecoHI.HiTracking.MergeTrackCollectionsHI_cff import *
+from RecoHI.HiTracking.hiJetCoreRegionalStep_cff import *
 
 from RecoHI.HiMuonAlgos.hiMuonIterativeTk_cff import *
+
+hiJetsForCoreTracking.cut = cms.string("pt > 50 && abs(eta) < 2.4")
+hiJetCoreRegionalStepSeeds.RegionFactoryPSet.RegionPSet.ptMin = cms.double( 10. )
+hiJetCoreRegionalStepTrajectoryFilter.minPt = 10.0
 
 hiTracking_noRegitMu = cms.Sequence(
     hiBasicTracking
     *hiDetachedTripletStep
     *hiLowPtTripletStep
     *hiPixelPairStep
+    *hiJetCoreRegionalStep 
     )
 
 hiTracking = cms.Sequence(
